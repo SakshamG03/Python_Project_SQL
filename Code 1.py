0be9729808700a2a20e942f0365c7048970412b3
Saksham <161133251+SakshamG03@users.noreply.github.com>
2024-09-27 22:35:07 +0530

    Change 2

---
+++ Code 1.py
@@ -0,0 +1,9 @@
+# These are the instructions for the project work.
+#
+# TOPIC: GROCERY MANAGEMENT SYSTEM\
+#
+# Day 1 Target:
+# Watch four videos on Grocery Management Project from the Playlist
+
+# Know the basic idea of the project and review the changes
+#
